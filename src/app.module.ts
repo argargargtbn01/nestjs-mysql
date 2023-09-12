91aa79659b55a32129d5191aa299d24f16bc2117
quangnm-sonat <quangnm@sonat.vn>
2023-09-12 09:12:00 +0700

    fix: del files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user/user.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
-      port: 3307,
-      username: 'root',
-      password: 'argargargtbn1',
-      database: 'nestjs_mysql',
+      // port: 3307,
+      // username: 'root',
+      // password: 'argargargtbn1',
+      // database: 'nestjs_mysql',
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
