2e83f0473628b1d568d1f521a604bd5c45e43cc1
quangnm-sonat <141127347+quangnm-sonat@users.noreply.github.com>
2023-12-28 17:47:36 +0700

    feat: upload with public url

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get, Param, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { Controller, Get, Param, Post, UseInterceptors, Put, UploadedFiles } from '@nestjs/common';
+import { FilesInterceptor } from '@nestjs/platform-express';
 import { UploadService } from './upload.service';
-@Controller('upload')
+@Controller('s3')
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
   @Get('files')
@@ -15,8 +15,38 @@ export class UploadController {
   }
 
   @Post('files')
-  @UseInterceptors(FileInterceptor('files'))
-  async uploadFile(@UploadedFile() file: Express.Multer.File): Promise<any> {
-    return await this.uploadService.uploadFile(file);
+  @UseInterceptors(FilesInterceptor('files'))
+  async uploadFiles(@UploadedFiles() files: Express.Multer.File[]): Promise<any> {
+    return await this.uploadService.uploadFiles(files);
+  }
+
+  @Get('buckets/acl')
+  async getBucketACL(): Promise<any> {
+    return await this.uploadService.getBucketACL();
+  }
+
+  @Put('buckets/acl')
+  async putBucketACL(): Promise<any> {
+    return await this.uploadService.putBucketACL();
+  }
+
+  @Get('buckets/cors-rules')
+  async getBucketCORSRules(): Promise<any> {
+    return await this.uploadService.getBucketCORSRules();
+  }
+
+  @Put('buckets/cors-rules')
+  async putBucketCORSRules(): Promise<any> {
+    return await this.uploadService.putBucketCORSRules();
+  }
+
+  @Get('buckets/policy')
+  async getBucketPolicy(): Promise<any> {
+    return await this.uploadService.getBucketPolicy();
+  }
+
+  @Put('buckets/policy')
+  async putBucketPolicy(): Promise<any> {
+    return await this.uploadService.putBucketPolicy();
   }
 }
